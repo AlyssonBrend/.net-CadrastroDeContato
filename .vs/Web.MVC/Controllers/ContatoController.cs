219687973c00af982288efd0310a110beeed273c
Alysson Brendo Araujo Canuto <alysson_brendo_12@hotmail.com>
2025-05-09 14:30:25 -0300

    2
    2

--- .vs/Web.MVC/Controllers/ContatoController.cs
+++ .vs/Web.MVC/Controllers/ContatoController.cs
@@ -8,5 +8,17 @@ namespace Web.MVC.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        { 
+            return View();
+        }
+        public IActionResult Apagar()
+        {   
+            return View();
+        }
     }
 }
